4710a156cbd7d40153dd462c4de44e03c6bfe6fe
Gabriel Wright <gabriel.wright@ryerson.ca>
2022-08-16 16:43:45 -0400

    refactor db connection into utility function

---
+++ backend/utility/setupDb.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const setupDbConnection = async (app) => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+		app.listen(process.env.PORT, () => {
+			console.log(
+				"connected to db & listening on port",
+				process.env.PORT
+			);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+module.exports = setupDbConnection;
