2aca1ad8148a4b66ad89ae42ed21fa523fe29219
Gabriel Wright <gabriel.wright@ryerson.ca>
2022-08-22 14:42:23 -0400

    triggering rebuild on heroku

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
 		)
 	);
 } else {
-	app.get("/", (req, res) => res.send("Please set to production"));
+	app.get("/", (req, res) => res.send("Please set to production mode"));
 }
 
 // connect to db
