a9f5b06d35c871fb1f3fdd3c2a8bca6aa783c3f8
Gabriel Wright <gabriel.wright@ryerson.ca>
2022-08-22 15:08:36 -0400

    removed utility function

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+const path = require("path");
 const express = require("express");
 const workoutRoutes = require("./routes/workoutRoutes");
 const userRoutes = require("./routes/userRoutes");
 const setupDbConnection = require("./utility/setupDb");
-const serveFrontend = require("./utility/serveFrontend");
 
 // express app
 const app = express();
@@ -19,19 +19,17 @@ app.use((req, res, next) => {
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/user", userRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "../frontend/build")));
-// 	app.get("*", (req, res) =>
-// 		res.sendFile(
-// 			path.resolve(__dirname, "../", "frontend", "build", "index.html")
-// 		)
-// 	);
-// } else {
-// 	app.get("/", (req, res) => res.send("Please set to production mode"));
-// }
-
 // server frontend
-serveFrontend(app);
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/build")));
+	app.get("*", (req, res) =>
+		res.sendFile(
+			path.resolve(__dirname, "../", "frontend", "build", "index.html")
+		)
+	);
+} else {
+	app.get("/", (req, res) => res.send("Please set to production mode"));
+}
 
 // connect to db
 setupDbConnection(app);
