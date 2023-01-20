7989884b7f1ab49c3007cbbdc1b6ca8dce795d76
Gabriel <gabriel.wright@ryerson.ca>
2023-01-19 22:28:19 -0500

    update proxy endpoint to new deploy api endpoint

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
 	app.use(
-		"/api",
+		'/api',
 		createProxyMiddleware({
-			target: "http://localhost:4000",
+			target: 'https://mern-workouts-redux-api.onrender.com',
 			changeOrigin: true,
 		})
 	);
