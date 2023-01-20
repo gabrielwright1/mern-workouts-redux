52c3fd52fa9e82caf36efec294356ad4d0cdc022
Gabriel <gabriel.wright@ryerson.ca>
2023-01-19 22:15:42 -0500

    setup app for render deploy

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const path = require("path");
-const express = require("express");
-const workoutRoutes = require("./routes/workoutRoutes");
-const userRoutes = require("./routes/userRoutes");
-const setupDbConnection = require("./utility/setupDb");
+const path = require('path');
+const express = require('express');
+const workoutRoutes = require('./routes/workoutRoutes');
+const userRoutes = require('./routes/userRoutes');
+const setupDbConnection = require('./utility/setupDb');
 
 // express app
 const app = express();
@@ -15,20 +15,8 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.use("/api/workouts", workoutRoutes);
-app.use("/api/user", userRoutes);
-
-// serve frontend
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/build")));
-	app.get("*", (req, res) =>
-		res.sendFile(
-			path.resolve(__dirname, "../", "frontend", "build", "index.html")
-		)
-	);
-} else {
-	app.get("/", (req, res) => res.send("Please set to production mode"));
-}
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
 // connect to db
 setupDbConnection(app);
